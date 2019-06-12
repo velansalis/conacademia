796d5d41ec66395263471eecdacd51f7a4e6d349
velansalis <velanmax8@gmail.com>
2019-06-12 17:37:12 +0530

    23. Optimised user auth

--- server/controllers/auth/user-auth.js
+++ server/controllers/auth/user-auth.js
@@ -11,8 +11,16 @@ const addActivity = async (username, context) => {
 		.exec();
 };
 
-const isOwnedBy = async (paramValue, username) => {
+const isOwnedBy = async username => {
 	try {
+		let paramValue = (() => {
+			if (context.params) {
+				return param.username || param.course_id;
+			} else {
+				return body.username || body.course_id;
+			}
+		})();
+
 		let user = await User.findOne({ username: paramValue })
 			.lean()
 			.exec();
@@ -33,59 +41,44 @@ const isOwnedBy = async (paramValue, username) => {
 	}
 };
 
-const fetchCredentials = context => {
-	let jwtValue = context.decoded.username;
-	let body = context.request.body;
-	let param = context.params;
-	let paramValue = (() => {
-		if (Object.keys(context.params) != 0) {
-			return param.username || param.course_id;
-		} else {
-			return body.username || body.course_id;
-		}
-	})();
-	return { jwtValue, paramValue };
-};
-
 const verifyUser = async (context, next) => {
 	try {
-		let { jwtValue, paramValue } = fetchCredentials(context);
-		console.log(jwtValue, paramValue);
-		await addActivity(jwtValue, context);
+		await addActivity(username, context);
+		let username = context.decoded.username;
 		switch (context.method) {
 			case "GET":
 				return next();
 			case "PUT":
-				if (await isOwnedBy(paramValue, jwtValue)) {
+				if (await isOwnedBy(paramValue, username)) {
 					return next();
 				} else {
-					let err = new Error(`Unauthorized method ${context.method} for ${jwtValue}`);
+					let err = new Error(`Unauthorized method ${context.method} for ${username}`);
 					err.name = "Unauthorized";
 					throw err;
 				}
 			case "PATCH":
-				if (await isOwnedBy(paramValue, jwtValue)) {
+				if (await isOwnedBy(paramValue, username)) {
 					return next();
 				} else {
-					let err = new Error(`Unauthorized method ${context.method} for ${jwtValue}`);
+					let err = new Error(`Unauthorized method ${context.method} for ${username}`);
 					err.name = "Unauthorized";
 					throw err;
 				}
 			case "DELETE":
-				if (await isOwnedBy(paramValue, jwtValue)) {
+				if (await isOwnedBy(paramValue, username)) {
 					return next();
 				} else {
-					let err = new Error(`Unauthorized method ${context.method} for ${jwtValue}`);
+					let err = new Error(`Unauthorized method ${context.method} for ${username}`);
 					err.name = "Unauthorized";
 					throw err;
 				}
 			case "POST":
-				let err = new Error(`Unauthorized method ${context.method} for ${jwtValue}`);
+				let err = new Error(`Unauthorized method ${context.method} for ${username}`);
 				err.name = "Unauthorized";
 				throw err;
 			default:
 				context.status = 401;
-				context.app.emit("error", new Error(`Unauthorized method ${context.method} for ${jwtValue}`), context);
+				context.app.emit("error", new Error(`Unauthorized method ${context.method} for ${username}`), context);
 		}
 	} catch (err) {
 		context.status = err.status || 500;

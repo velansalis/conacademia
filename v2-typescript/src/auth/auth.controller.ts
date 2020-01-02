d1f8c4781cc4391351007cbcfd09ad86b24c5e2e
velansalis <velansalis@tuta.io>
2020-01-02 11:11:35 +0530

    Added documentation

--- v2-typescript/src/auth/auth.controller.ts
+++ v2-typescript/src/auth/auth.controller.ts
@@ -2,12 +2,19 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDTO } from '../modules/user/user.dto';
 
+/**
+ * This class handles the authentication of the users.
+ */
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    /**
+     * Handles the login of the user
+     * @param userdata Contains username and password as an object
+     */
     @Post('login')
-    async loginUser(@Body() userdata: Partial<UserDTO>): Promise<object> {
+    async login(@Body() userdata: Partial<UserDTO>): Promise<object> {
         let response = await this.authService.loginUser(userdata);
         return {
             message: 'Authentication successful',
@@ -15,6 +22,10 @@ export class AuthController {
         };
     }
 
+    /**
+     * Handles the registeration of new user to the system.
+     * @param userdata Contains username, password, fname, lname, dob of the user
+     */
     @Post('register')
     async register(@Body() userdata: UserDTO): Promise<object> {
         let response = await this.authService.registerUser(userdata);
@@ -24,6 +35,10 @@ export class AuthController {
         };
     }
 
+    /**
+     * Handles the deletion of the already existing user from the system.
+     * @param userdata Contains the username and password of the user.
+     */
     @Post('terminate')
     async delete(@Body() userdata: Partial<UserDTO>): Promise<object> {
         let response = await this.authService.deleteUser(userdata);
